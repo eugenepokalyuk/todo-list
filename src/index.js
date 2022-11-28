5946abc141a0c909b8017e7c2abbd7468cc9b8c8
Eugene <eugene.pokalyuk@gmail.com>
2022-11-28 11:50:21 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
-// <img src="https://getuikit.com/assets/uikit/src/images/icons/pencil.svg" width="40" height="40" alt='' data-uk-svg />
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
